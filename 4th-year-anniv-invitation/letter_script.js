6e47e26445dac451c962d0f0e1ece01bb8f32c6a
itschanchan <sean.andrei99@gmail.com>
2025-01-07 19:30:45 +0800

    Delete 4th-year-anniv-invitation directory

--- 4th-year-anniv-invitation/letter_script.js
+++
@@ -1,9 +0,0 @@
-// Function to open the popup
-function openPopup() {
-    document.getElementById("popupForm").style.display = "flex";
-}
-
-// Function to close the popup
-function closePopup() {
-    document.getElementById("popupForm").style.display = "none";
-}
